bcf7c63d60598e9c676dcc6ad767a893071b26e6
Marco Amador <marcoamador214@gmail.com>
2022-11-14 10:28:43 -0800

    Created Initial Components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header'
+import NavBar from './NavBar'
+import Search from './Search'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NavBar />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import MainList from './MainList'
+import ReviewList from './ReviewList'
+import SaleList from './SaleList'
+
+function NavBar() {
+    return (
+        <div>
+            <button>Main</button>
+            <button>Review</button>
+            <button>Sale</button>
+        </div>
+    )
+}
+
+export default NavBar;
