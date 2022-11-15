7b86a6aca71e8bab148d9b9a0b44702dc1ae6398
Conrad Etherington <conrad.etherington@gmail.com>
2022-11-15 10:45:34 -0700

    Trying to get react router up and running, issue with page change only happening on refresh

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,32 @@ import NavBar from './NavBar'
 //import ReviewCard from './ReviewCard';
 import Search from './Search'
 import MainList from './MainList';
-import { Routes, Route } from "react-router-dom"
+import SaleList from './SaleList';
+import { Switch, Route } from "react-router-dom"
 import ReviewList from "./ReviewList"
+import {useState} from 'react'
 
 function App() {
+const [renderState, setRenderState] = useState(true)
+
   return (
     <div className="App">
       <Header />
-      <NavBar />
       <Search />
-      <Routes>
+      <NavBar renderState={renderState} setRenderState={setRenderState} />
+      <Switch>
         <Route path="/reviews">
             <ReviewList />
         </Route>
+        <Route path="/sales">
+            <SaleList />
+          </Route>
           <Route path="/">
             <MainList />
           </Route>
 
-        </Routes>
+
+        </Switch>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,22 @@
 import {NavLink} from 'react-router-dom'
 
 
-function NavBar() {
+function NavBar({setRenderState, renderState}) {
     return (
        <nav className='nav'>
         <ul>
             <li>
-                <NavLink to="/">All Trailers</NavLink>
+                <NavLink to="/" onClick={() => setRenderState(!renderState)}>All Trailers</NavLink>
             </li>
             <li>
-                <NavLink to="/sales">For Sale</NavLink>
+                <NavLink onClick={() => setRenderState(!renderState)} to="/sales">For Sale</NavLink>
             </li>
             <li>
-                <NavLink to="/rent"> For Rent </NavLink>
+                <NavLink onClick={() => setRenderState(!renderState)} to="/rent"> For Rent </NavLink>
                 {/* Redirect to home page with message saying rent coming soon if we cant meet goal.*/}
             </li>
             <li>
-            <NavLink to="/review">Post Your Own</NavLink>
+            <NavLink onClick={() => setRenderState(!renderState)} to="/reviews">Post Your Own</NavLink>
             </li>
         </ul>
        </nav> 
