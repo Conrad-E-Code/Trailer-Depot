eb39694f7e3dab7ad3c88e3fd74d2320a20c102b
william Rodriguez-Ledesma <popolol.wr.wr@gmail.com>
2022-11-15 16:30:18 -0800

    setup fetch for trailerData, mapped over data to ReviewCard

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,33 @@ import MainList from './MainList';
 import SaleList from './SaleList';
 import { Routes, Route } from "react-router-dom"
 import NewForm from "./NewForm"
-import {useState} from 'react'
-
+import {useState, useEffect} from 'react'
+import ReviewCard from "./ReviewCard"
 function App() {
-const [renderState, setRenderState] = useState(true)
 
+  const [initialData, setInitialData] = useState([])
+
+useEffect(() =>{
+  fetch( 'http://localhost:3001/trailers')
+  .then(r=> r.json())
+  .then(trailerData => {setInitialData(trailerData)})
+
+},[])
+
+const mappedTrailers = initialData.map(trailer=>{
+  return <ReviewCard key={trailer.name} price={trailer.price} forSale={trailer['for-sale']} name={trailer.name} imageAlbum={trailer['image-album']} />})
+  
+  
   return (
     <div className="App">
       <Header />
       <Search />
-      <NavBar renderState={renderState} setRenderState={setRenderState} />
+      <NavBar  />
+      
       <Routes>
         <Route element={<NewForm />} path="/reviews"/>
         <Route element={<SaleList />} path="/sales"/>
-        <Route element={<MainList />} path="/"/>
+        <Route element={<MainList mappedTrailers={mappedTrailers} />} path="/"/>
 
 
         </Routes>
