db21f06226b0cb04a6da8e7f9af7a4b7327eeb24
Marco Amador <marcoamador214@gmail.com>
2022-11-16 12:45:49 -0800

    further css styling

--- src/components/TrailerCard.js
+++ src/components/TrailerCard.js
@@ -13,11 +13,11 @@ function TrailerCard({ trailers }) {
     
     return (
         <li className='card-item'>
-            <div className='trailer-card' style={{  cursor: 'pointer', width: '18rem' }} onClick={handleClick}>
-                <img className='card-image' variant="top" src={trailers.image} height='200px' width='225px'/>
-                <h2>{trailers.name}</h2>
-                {showReview ? <p>Review: {trailers.review}</p> : ''}
-                <ListGroup.Item>Price: {trailers.for_sale ? "$" + trailers.price : "Not For Sale"}</ListGroup.Item>
+            <div className='trailer-card' style={{  cursor: 'pointer', width: '18rem' }}>
+                <img onClick={handleClick} className='card-image' variant="top" src={trailers.image} height='200px' width='225px'/>
+                <h2 className='card-name'>{trailers.name}</h2>
+                {showReview ? <p className='card-review'>Review: {trailers.review}</p> : ''}
+                <ListGroup.Item className='card-price'>Price: {trailers.for_sale ? "$" + trailers.price : "Not For Sale"}</ListGroup.Item>
                 <ListGroup.Item className='location'>{trailers.location}</ListGroup.Item>
             </div>
         </li>
